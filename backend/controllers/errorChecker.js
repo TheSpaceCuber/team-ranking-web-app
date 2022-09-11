7b63626d045038b79e855f48d22a0faf88dda2f4
Royce Ho <royce963@gmail.com>
2022-09-11 21:29:38 +0800

    Create errorChecker.js
    Validates input from frontend

---
+++ backend/controllers/errorChecker.js
@@ -0,0 +1,45 @@
+// const teamsInfo = [
+//     'firstTeam 17/05 2',
+//     'secondTeam 07/02 2',
+//     'thirdTeam 24/04 1',
+//     'fourthTeam 24/01 1',
+//     ''
+// ]
+
+// const matchResults = [ 'firstTeam secondTeam 0 3', 'thirdTeam fourthTeam 1 1', '' ]
+
+/**
+ * Checks if team info input is valid
+ * @param {*} input 
+ * @returns 
+ */
+export const isValidTeamInfo = (input) => {
+    for (const entry of input) {
+        if (entry.trim() == "") { continue }
+        if (entry.split(" ").length !== 3) { throw new Error("Invalid team info, wrong syntax for line: " + entry) }
+        const [teamName, regDate, groupNum] = entry.split(" ")
+        if (groupNum != 1 && groupNum != 2) { throw new Error("Invalid team info, Invalid Group Number for line: " + groupNum) }
+        const regDateRegex = /^\d{2}\/\d{2}$/
+        if (!regDateRegex.test(regDate)) { throw new Error("Invalid team info, Invalid Date for line: " + regDate) }
+    }
+    return true
+}
+
+
+/**
+ * Checks if match result input is valid
+ * @param {} input 
+ * @returns 
+ */
+export const isValidMatchResult = (input) => {
+    for (const entry of input) {
+        if (entry.trim() == "") { continue }
+        if (entry.split(" ").length !== 4) { throw new Error("Invalid match result, wrong syntax for line: " + entry) }
+        const [firstTeamName, secondTeamName, firstTeamScore, secondTeamScore] = entry.split(" ")
+        const postiveNumStringRegex = /^\d+$/
+        if (!(postiveNumStringRegex.test(firstTeamScore) && postiveNumStringRegex.test(secondTeamScore))) {
+            throw new Error("Invalid team info, Invalid Date for line: " + regDate)
+        }
+    }
+    return true
+}
